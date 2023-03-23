cdb862b28e0186ec30080c1888ee828c682b42cc
KhetwalDevesh <deveshkhetwal05@gmail.com>
2023-03-23 12:09:09 +0530

    added payment success

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 const Success = () => {
-  return (
-    <div className="flex items-center justify-center h-screen">Success</div>
-  );
+	return (
+		<div className="flex items-center text-3xl justify-center min-h-screen bg-[#fff1e5]">
+			<div>
+				<span className="text-4xl font-bold">Yay! </span>{" "}
+				<span> Order created successfully.</span>
+			</div>
+		</div>
+	);
 };
 
 export default Success;
